af5e1b337502b53196891905fbc08f79933d8c03
angelo <angelorefilguerrero@gmail.com>
2026-03-12 15:37:21 +0800

    html pang bading

---
+++ node.js
@@ -0,0 +1,34 @@
+const display = document.getElementById("display");
+
+function appendToDisplay(input) {
+    display.value += input;
+}
+
+function clearDisplay() {
+    display.value = "";
+}
+
+function calculate() {
+    try {
+        let expression = display.value.replace(/²/g, "**2");
+        expression = expression.replace(/√(\d+(\.\d+)?)/g, "Math.sqrt($1)");
+        display.value = eval(expression);
+    }
+    catch(error) {
+        display.value = "error";
+    }
+}
+
+function removeNum() {
+    display.value = display.value.slice(0, -1);
+} 
+
+function squared() {
+    display.value += '²';
+
+}
+
+function sqrt(){
+    display.value +='√';
+}
+
